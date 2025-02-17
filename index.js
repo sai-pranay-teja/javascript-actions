9da5eabe1bfce426e2f52518c34e4cb7e838e55c
sai pranay teja <36685961+sai-pranay-teja@users.noreply.github.com>
2025-02-17 15:45:03 +0000

    java script actions started

---
+++ index.js
@@ -0,0 +1,15 @@
+import * as core from '@actions/core';
+import * as core from '@actions/github';
+
+
+try {
+    const name = core.getInput('name');
+    const greeting_value=`Hello ${name}`;
+    core.setOutput('greeting', greeting_value);
+
+  } 
+  
+  
+  catch (error) {
+    core.setFailed(error.message);
+  }
