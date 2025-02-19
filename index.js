fec39f0efabcab14b1b321247dde6a722e8fe30c
sai pranay teja <36685961+sai-pranay-teja@users.noreply.github.com>
2025-02-19 13:21:03 +0000

    my final stuff

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import path from 'path';
 try {
   
     const indexPath = path.join(__dirname, 'dist', 'index.js');
+    console.log('indexPath:', indexPath); 
 
     const name = core.getInput('name');
     const greeting_value=`Hello ${name}`;
