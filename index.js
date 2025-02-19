77005a8f62395d13321b94fbb5116b6bdb8a8e23
sai pranay teja <36685961+sai-pranay-teja@users.noreply.github.com>
2025-02-19 13:01:04 +0000

    this can be my last

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import * as github from '@actions/github';
 
 
 try {
+
+    const path = require('path');
+    const indexPath = path.join(__dirname, 'dist', 'index.js');
+
     const name = core.getInput('name');
     const greeting_value=`Hello ${name}`;
     core.setOutput('greeting', greeting_value);
@@ -16,4 +20,3 @@ try {
   catch (error) {
     core.setFailed(error.message);
   }
-  
