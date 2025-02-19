5fa2fd4d42b5b38c24eef0caa0052be2b1c06161
sai pranay teja <36685961+sai-pranay-teja@users.noreply.github.com>
2025-02-19 13:11:39 +0000

    my final stuff

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
+import path from 'path';
+
 
 
 try {
-
-    const path = require('path');
+  
     const indexPath = path.join(__dirname, 'dist', 'index.js');
 
     const name = core.getInput('name');
