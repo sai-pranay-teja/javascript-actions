d53158f1c4e966422dd39d06fd6b6f063e493921
sai pranay teja <36685961+sai-pranay-teja@users.noreply.github.com>
2025-02-20 06:39:00 +0000

    practise change

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
-// import path from 'path';
-
-
 
 try {
   
-    // const indexPath = path.join(__dirname, 'dist', 'index.js');
-    // console.log('indexPath:', indexPath); 
-
     const name = core.getInput('name');
     const greeting=`Hello ${name}`;
     core.setOutput('greeting', greeting);
 
-  } 
-  
-
-  
+  }  
   
   
   catch (error) {
